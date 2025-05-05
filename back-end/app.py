f7415866b422bab95e99d70a9efb8c7eb5f11477
shilamy <macmynev@gmail.com>
2025-05-05 15:48:49 +0300

    fixing auth api / adding dashboard route in the login form

--- back-end/app.py
+++ back-end/app.py
@@ -1,11 +1,18 @@
+import os
 from flask import Flask
 from flask_migrate import Migrate
-from models.user import db
+from models.user import db, User
 from routes.auth import auth
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///transport.db"
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, "instance", "transport.db")
+
+os.makedirs(os.path.join(basedir, "instance"), exist_ok=True)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
@@ -13,9 +20,9 @@ migrate = Migrate(app, db)
 CORS(app)
 app.register_blueprint(auth, url_prefix="/api")
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
+    # Create the database and tables if they don't exist
+    with app.app_context():
+        db.create_all()
     app.run(port=8000, debug=True)
     
